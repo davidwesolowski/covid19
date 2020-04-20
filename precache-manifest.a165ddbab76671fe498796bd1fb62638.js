6181335da3cb47dd0637f2106376b666f8383732
Dawid Wesołowski <s169122@student.pg.edu.pl>
2020-04-20 18:43:49 +0200

    Updates

---
+++ precache-manifest.a165ddbab76671fe498796bd1fb62638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ce0384c9478f6a09833c08160f2dde",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "6a9309bc9c10444e21bd",
+    "url": "/covid19/static/js/2.e4f48016.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covid19/static/js/2.e4f48016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0efec82b43dabe1ef442",
+    "url": "/covid19/static/js/main.d11d6a1a.chunk.js"
+  },
+  {
+    "revision": "3401f1b486f59c1eebc9",
+    "url": "/covid19/static/js/runtime-main.46e8034d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/logo.d7265326.png"
+  }
+]);
